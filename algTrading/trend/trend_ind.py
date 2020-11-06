3cc566e9f01afb2da3f10aca36a57740f0966df4
Hendrik Motmans <hendrikmotmans@hotmail.com>
2020-11-06 15:16:45 +0100

    bollinger bands returns moving average

--- algTrading/trend/trend_ind.py
+++ algTrading/trend/trend_ind.py
@@ -8,13 +8,13 @@ function moving_average
 
     output: -array of same length as data array, but with moving average
 """
-def moving_average(data, length):
+def moving_average(data, period):
     result = []
     for i in range(len(data)):
-        if i < length-1:
+        if i < period-1:
             result.append(mean(data[0:i+1]))
         else:
-            result.append(mean(data[i-length+1:i+1]))
+            result.append(mean(data[i-period+1:i+1]))
             
     return result
 
@@ -25,13 +25,13 @@ function moving_standard_deviation
 
     output: -array of same length as data array, but with moving standard deviation
 """
-def moving_standard_deviation(data, length):
+def moving_standard_deviation(data, period):
     result = [0]
     for i in range(len(data)-1):
-        if i < length - 2:
+        if i < period - 2:
             result.append(stdev(data[0:i+2]))
         else:
-            result.append(stdev(data[i-length+2:i+2]))
+            result.append(stdev(data[i-period+2:i+2]))
             
     return result
 
@@ -41,13 +41,14 @@ function bollinger_bands
             - moving length
             - band factor
 
-    output: -2 arrays of same length as data array, but with bollinger bands (upper bollinger band, lower bollinger band)
+    output: - Moving average
+            - 2 arrays of same length as data array, but with bollinger bands (upper bollinger band, lower bollinger band)
 """
-def bollinger_bands(data, length, bf):
-    MA = moving_average(data, length)
-    SDEV = moving_standard_deviation(data, length)
+def bollinger_bands(data, period, bf):
+    MA = moving_average(data, period)
+    SDEV = moving_standard_deviation(data, period)
 
     upper_bollinger_band = [x + bf*y for (x,y) in zip(MA, SDEV)]
     lower_bollinger_band = [x - bf*y for (x,y) in zip(MA, SDEV)]
 
-    return upper_bollinger_band, lower_bollinger_band
+    return MA, upper_bollinger_band, lower_bollinger_band
--- test.py
+++ test.py
@@ -4,8 +4,8 @@ import algTrading.trend.trend_ind as trend
 print('test test')
 
 test_array = [1,2,3,4,5,6,7,8,9,4,5,8,7,4,5,2,2,1,4,5,8]
-result1, result2 = trend.bollinger_bands(test_array,3, 1)
-result3 = trend.moving_average(test_array,3)
+result3, result1, result2 = trend.bollinger_bands(test_array,3, 1)
+#result3 = trend.moving_average(test_array,3)
 
 for i in range(len(result1)):
     print(i+1,result2[i], result3[i], result1[i])
