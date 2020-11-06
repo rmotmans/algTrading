93953100d46c1af47166ded4c732f9f5e4c3fa20
Hendrik Motmans <hendrikmotmans@hotmail.com>
2020-11-06 15:06:48 +0100

    temporary commit RSI

--- algTrading/momentum/momentum_ind.py
+++ algTrading/momentum/momentum_ind.py
@@ -1,3 +1,15 @@
 
-def test_function():
-    print('test')
+
+def RSI(data, period):
+    U = [0]
+    D = [0]
+    for i in range(len(data)-1):
+        diff = data[i+1] - data[i]
+        if diff >= 0:
+            U.append(diff)
+            D.append(0)
+        else:
+            U.append(0)
+            D.append(-diff)
+    
+    
